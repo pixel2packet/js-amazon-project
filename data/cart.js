db1ae542ee80a42482f76d121c818d8543695ea7
pixel2packet <pritesh@intelxlabs.com>
2025-07-28 15:10:18 +0530

    Sace cart items to localstorage

--- data/cart.js
+++ data/cart.js
@@ -1,10 +1,19 @@
-export let cart = [{ // deduplication or normalizing data, no need to resave the same data
-    productId: `e43638ce-6aa0-4b85-b27f-e1d07eb678c6`,
-    quantity: 2,
-}, {
-    productId: `15b6fc6f-327a-4ec4-896f-486349e85a3d`,
-    quantity: 1,
-}];
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+
+if (!cart) {
+    cart = [{ // deduplication or normalizing data, no need to resave the same data
+        productId: `e43638ce-6aa0-4b85-b27f-e1d07eb678c6`,
+        quantity: 2,
+    }, {
+        productId: `15b6fc6f-327a-4ec4-896f-486349e85a3d`,
+        quantity: 1,
+    }];
+}
+
+function saveToStorage() {
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 export function addToCart(productId) {
     let matchingItem;
@@ -28,6 +37,8 @@ export function addToCart(productId) {
             quantity: Number(selectedValue)
         });
     }
+
+    saveToStorage();
 }
 
 export function removeFromCart(productId) {
@@ -40,4 +51,6 @@ export function removeFromCart(productId) {
     });
 
     cart = newCart;
+
+    saveToStorage();
 }
