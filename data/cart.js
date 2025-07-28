96a90941120fd9795163c721ffe75a83d64564d9
pixel2packet <pritesh@intelxlabs.com>
2025-07-28 14:16:34 +0530

    Generate HTML for summary

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,31 @@
-export const cart = [];
+export const cart = [{ // deduplication or normalizing data, no need to resave the same data
+    productId: `e43638ce-6aa0-4b85-b27f-e1d07eb678c6`,
+    quantity: 2,
+}, {
+    productId: `15b6fc6f-327a-4ec4-896f-486349e85a3d`,
+    quantity: 1,
+}];
+
+export function addToCart(productId) {
+    let matchingItem;
+
+    cart.forEach((cartItem) => {
+        if (productId === item.productId) {
+            matchingItem = cartItem;
+        }
+    });
+
+    const selectElement = document.getElementById(`js-quantity-selector-${productId}`)
+
+    const selectedValue = selectElement.value;
+    console.log('selectedValue', selectedValue)
+
+    if (matchingItem) {
+        matchingItem.quantity += Number(selectedValue);
+    } else {
+        cart.push({
+            productId: productId,
+            quantity: Number(selectedValue)
+        });
+    }
+}
