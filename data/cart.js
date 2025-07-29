84019596b506f9d76ed82fd196a846eb8278c4fa
pixel2packet <pritesh@intelxlabs.com>
2025-07-29 15:34:38 +0530

    Save data and generate HTML for delivery options

--- data/cart.js
+++ data/cart.js
@@ -5,9 +5,11 @@ if (!cart) {
     cart = [{ // deduplication or normalizing data, no need to resave the same data
         productId: `e43638ce-6aa0-4b85-b27f-e1d07eb678c6`,
         quantity: 2,
+        deliveryOptionId: '1',
     }, {
         productId: `15b6fc6f-327a-4ec4-896f-486349e85a3d`,
         quantity: 1,
+        deliveryOptionId: '2',
     }];
 }
 
@@ -34,9 +36,10 @@ export function addToCart(productId) {
     } else {
         cart.push({
             productId: productId,
-            quantity: Number(selectedValue)
+            quantity: Number(selectedValue),
+            deliveryOptionId: '1',
         });
-    }
+    } 
 
     saveToStorage();
 }
